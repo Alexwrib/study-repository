83764324d93f51dc9dd525eafb83b5981dcd3c67
Alex <alex.alexanyan1306@gmail.com>
2026-06-27 14:35:04 +0400

    add feature 2

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	feature1 "study/feature1"
+	"study/feature2"
 )
 
 func main() {
 	fmt.Println("Hello git!")
 	feature1.Feature1()
 
+	feature2.Feature2()
 
 	fmt.Println("Gelo belo")
 }
